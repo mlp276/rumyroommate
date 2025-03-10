94d520f9ca2bb6a592777d639cb769ac16a43e5e
Miguel Pagador <miguel.pagador@rutgers.edu>
2025-03-09 22:25:40 -0400

    Created a JS Module for creating a connection to the local SQL database

---
+++ databaseserver/connect_db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "yourSQLUser",
+    password: "yourSQLPassword",
+    database: "demo"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
--- databaseserver/server.js
+++ databaseserver/server.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const url = require('url');
 
-http.createServer(function (req, res) {
+http.createServer((req, res) => {
+    const pathname = url.parse(req.url, true).pathname;
+    const query = url.parse(req.url, true).query;
+    
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
+    res.write('URI: ' + pathname + '\n');
+
+    res.write('Queries:\n');
+    for (const [key, value] of Object.entries(query)) {
+        res.write(`${key}: ${value}` + '\n');
+    }
+
     res.end();
 }).listen(8080);
