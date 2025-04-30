acfd4e559d4f5a201d127f0020af367e82c78930
Mariana0101 <125153206+Mariana0101@users.noreply.github.com>
2025-04-29 23:19:44 -0400

    Update database.js

--- database.js
+++ database.js
@@ -10,17 +10,61 @@ const pool = mysql.createPool({
 }).promise()
 
 export async function getListings(){
-    const [rows] = await pool.query("SELECT * FROM Listings")
+    const [rows] = await pool.query("SELECT * , p.female, p.male, p.other, p.smoker, p.non_smoker, p.clean FROM Listings l LEFT JOIN Preferences p ON l.post_id = p.post_id")
     return rows;
 }
 
 export async function getListing(post_id){
-    const [rows] = await pool.query('SELECT * FROM Listings WHERE post_id = ?', [post_id])
+    const [rows] = await pool.query(`SELECT l.*, p.female, p.male, p.other, p.smoker, p.non_smoker, p.clean FROM Listings l LEFT JOIN Preferences p ON l.post_id = p.post_id WHERE l.post_id = ?`, [post_id])
     return rows
 }
 
-export async function createListing(title, bio, num_rooms){
-    const [result] = await pool.query('INSERT INTO Listings (title, bio, num_rooms) VALUES (?, ?, ?)', [title, bio, num_rooms])
-    const post_id = result.insertId
-    return getListing(post_id)
+export async function createListing(listing, preferences) {
+    const conn = await pool.getConnection()
+    try {
+        await conn.beginTransaction()
+
+        const {
+            title, campus, house_address, house_type,
+            num_rooms, num_bath, roommates, rent,
+            dt_available, bio
+        } = listing
+
+        const [listingResult] = await conn.query(
+            `INSERT INTO Listings 
+            (title, campus, house_address, house_type, num_rooms, num_bath, roommates, rent, dt_available, bio) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [title, campus, house_address, house_type, num_rooms, num_bath, roommates, rent, dt_available, bio]
+        )
+
+        const post_id = listingResult.insertId
+
+        const {
+            female = false,
+            male = false,
+            other = false,
+            smoker = false,
+            non_smoker = false,
+            clean = null
+        } = preferences
+
+        await conn.query(
+            `INSERT INTO Preferences 
+            (post_id, female, male, other, smoker, non_smoker, clean) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+            [post_id, female, male, other, smoker, non_smoker, clean]
+        )
+
+        await conn.commit()
+
+        const [result] = await conn.query(
+            `SELECT * FROM Listings WHERE post_id = ?`, [post_id]
+        )
+        return result
+    } catch (err) {
+        await conn.rollback()
+        throw err
+    } finally {
+        conn.release()
+    }
 }
