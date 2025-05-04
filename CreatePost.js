a4450ac6e6be3edeec39d62181faf160be4ced6b
Asaini434 <as4341@scarletmail.rutgers.edu>
2025-05-04 15:17:07 -0400

    Update CreatePost.js

--- CreatePost.js
+++ CreatePost.js
@@ -1,6 +1,22 @@
 const form = document.querySelector("form");
 const submitBtn = document.querySelector("#postBtn");
 const allInputs = form.queryselectorAll("input, select, textarea");
+const apiKey = '6a87dedbc88053347a5a49405418396d';
+const elements = document.getElementsByClassName("add");
+const query = element; 
+const endpoint = `http://api.positionstack.com/v1/forward?access_key=${apiKey}&query=${query}`; 
+
+async function getGeocodingData() {
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+getGeocodingData();
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
